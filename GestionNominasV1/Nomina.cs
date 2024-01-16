0538d95a970edeca88a3ce2cd890eda70f499c15
DAM 1 <DAM 1@DESKTOP-EN3MTPA>
2024-01-16 09:32:38 +0100

    Creación de la Propiedad NombreEmpleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -14,5 +14,32 @@ namespace GestionNominasV1
         private string? _puesto;
         private int _horas;
         private float _salarioHora;
+
+        // PROPIEDADES
+
+        /// <summary>
+        /// Nombre del Empleado
+        /// </summary>
+        public string? NombreEmpleado
+        {
+            get
+            {
+                // Control de Contenido
+                if (string.IsNullOrEmpty(_nombre)) 
+                    throw new Exception("Nombre del trabajador no establecido");
+                
+                // Devolución del Valor
+                return _nombre;
+            }
+            set
+            {
+                // Validación de los Datos a Establecer
+                if (string.IsNullOrEmpty(value)) throw new Exception("El Nombre del Trabajador " +
+                    "no puede ser null o la cadena vacía");
+
+                // Establecimiento del valor al miembro privado
+                _nombre = value;
+            }
+        }
     }
 }
